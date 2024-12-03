235e47422ac0c2d618eb8734f6b887d52c0d674f
Andrew Kalz <andrewkalz@gmail.com>
2024-12-03 17:18:09 -0600

    adding all day to import on main and an input to run any day

--- advent_of_code_2024/__init__.py
+++ advent_of_code_2024/__init__.py
@@ -1,10 +1,54 @@
 from advent_of_code_2024.day_01 import Day1
 from advent_of_code_2024.day_02 import Day2
 from advent_of_code_2024.day_03 import Day3
+from advent_of_code_2024.day_04 import Day4
+from advent_of_code_2024.day_05 import Day5
+from advent_of_code_2024.day_06 import Day6
+from advent_of_code_2024.day_07 import Day7
+from advent_of_code_2024.day_08 import Day8
+from advent_of_code_2024.day_09 import Day9
+from advent_of_code_2024.day_10 import Day10
+from advent_of_code_2024.day_11 import Day11
+from advent_of_code_2024.day_12 import Day12
+from advent_of_code_2024.day_13 import Day13
+from advent_of_code_2024.day_14 import Day14
+from advent_of_code_2024.day_15 import Day15
+from advent_of_code_2024.day_16 import Day16
+from advent_of_code_2024.day_17 import Day17
+from advent_of_code_2024.day_18 import Day18
+from advent_of_code_2024.day_19 import Day19
+from advent_of_code_2024.day_20 import Day20
+from advent_of_code_2024.day_21 import Day21
+from advent_of_code_2024.day_22 import Day22
+from advent_of_code_2024.day_23 import Day23
+from advent_of_code_2024.day_24 import Day24
+from advent_of_code_2024.day_25 import Day25
 
 
 __all__ = [
-    Day1,
-    Day2,
-    Day3,
+    "Day1",
+    "Day2",
+    "Day3",
+    "Day4",
+    "Day5",
+    "Day6",
+    "Day7",
+    "Day8",
+    "Day9",
+    "Day10",
+    "Day11",
+    "Day12",
+    "Day13",
+    "Day14",
+    "Day15",
+    "Day16",
+    "Day17",
+    "Day18",
+    "Day19",
+    "Day20",
+    "Day21",
+    "Day22",
+    "Day23",
+    "Day24",
+    "Day25",
 ]
--- advent_of_code_2024/main.py
+++ advent_of_code_2024/main.py
@@ -1,8 +1,39 @@
-from advent_of_code_2024 import Day3
+from advent_of_code_2024 import *
+
+
+days = {
+    "1": Day1,
+    "2": Day2,
+    "3": Day3,
+    "4": Day4,
+    "5": Day5,
+    "6": Day6,
+    "7": Day7,
+    "8": Day8,
+    "9": Day9,
+    "10": Day10,
+    "11": Day11,
+    "12": Day12,
+    "13": Day13,
+    "14": Day14,
+    "15": Day15,
+    "16": Day16,
+    "17": Day17,
+    "18": Day18,
+    "19": Day19,
+    "20": Day20,
+    "21": Day21,
+    "22": Day22,
+    "23": Day23,
+    "24": Day24,
+    "25": Day25,
+}
 
 
 if __name__ == "__main__":
-    d = Day3()
+    print("Enter day to run: (e.g. 8)")
+    day = input()
+    d = days[day]()
     print(f"== {str(d)} ====================")
     print(f"    part 1: {d.part_1()}")
     print(f"    part 2: {d.part_2()}")

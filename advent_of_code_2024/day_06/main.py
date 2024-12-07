80950e3283ee134765d9c97e7fb65268bf974bfd
Andrew Kalz <andrewkalz@gmail.com>
2024-12-07 12:28:03 -0600

    day 7 done, working on 6 still

--- advent_of_code_2024/day_06/main.py
+++ advent_of_code_2024/day_06/main.py
@@ -1,3 +1,4 @@
+import copy
 from advent_of_code_2024.day import Day
 
 
@@ -6,6 +7,19 @@ class Direction:
     down = (0, 1)
     left = (-1, 0)
     right = (1, 0)
+
+
+def replace_char_at_index(string, index, new_char):
+    """Replaces the character at a specific index in a string with a new character."""
+
+    # Convert the string to a list of characters
+    char_list = list(string)
+
+    # Replace the character at the specified index
+    char_list[index] = new_char
+
+    # Join the list of characters back into a string
+    return "".join(char_list)
 
 
 class Day6(Day):
@@ -32,23 +46,39 @@ class Day6(Day):
         if current_dir == Direction.left:
             return Direction.up
 
-    def _on_map(self, pos: tuple[int, int]) -> bool:
+    def _on_map(self, pos: tuple[int, int], map: list[list[str]] = None) -> bool:
+        if not map:
+            map = self.input_data
         return all([
-            0 <= pos[0] < len(self.input_data[0]),
-            0 <= pos[1] < len(self.input_data),
+            0 <= pos[0] < len(map[0]),
+            0 <= pos[1] < len(map),
         ])
 
-    def _move(self, current_dir: tuple[int, int], current_pos: tuple[int, int]) -> tuple[tuple[int, int], tuple[int, int]]:
-        new_pos = self._add_tuple(current_pos, current_dir)
-        if not self._on_map(new_pos):
-            pass
-        elif self.input_data[new_pos[1]][new_pos[0]] == "#":
-            current_dir = self._turn_right(current_dir)
-            new_pos = self._add_tuple(current_pos, current_dir)
-        return current_dir, new_pos
+    def _move(self, dir: tuple[int, int], pos: tuple[int, int], map=None) -> tuple[tuple[int, int], tuple[int, int]]:
+        if not map:
+            map = self.input_data
+        new_pos = self._add_tuple(pos, dir)
+        if self._on_map(new_pos, map=map) and map[new_pos[1]][new_pos[0]] == "#":
+            dir = self._turn_right(dir)
+            new_pos = pos
+        return dir, new_pos
 
     def _add_tuple(self, t1: tuple[int, int], t2: tuple[int, int]) -> tuple[int, int]:
         return t1[0] + t2[0], t1[1] + t2[1]
+
+    def _is_loop(self, _map: list[list[str]], dir: tuple[int, int], pos: tuple[int, int]) -> bool:
+        # print("is loop check")
+        # for line in _map:
+        #     print(line)
+        visited_pos = set()
+        while self._on_map(pos, map=_map):
+            visited_pos.add((dir, pos))
+            dir, pos = self._move(dir, pos, map=_map)
+            if (dir, pos) in visited_pos:
+                # print("yay")
+                return True
+        # print("boo")
+        return False
 
     def part_1(self):
         visited_pos = set()
@@ -60,20 +90,30 @@ class Day6(Day):
         return len(visited_pos)  # 4665
 
     def part_2(self):
-        visited_pos = set()
-        obstructions_placed = 0
+        obstructions_placed = []
         current_dir = Direction.up
         current_pos = self._start_pos
         while self._on_map(current_pos):
-            visited_pos.add(current_pos)
+            # always try to block the guard's path on each move
+            obstruction = self._add_tuple(current_pos, current_dir)
+            if not self._on_map(obstruction) or self.input_data[obstruction[1]][obstruction[0]] == "#":
+                pass
+            else:
+                map = copy.deepcopy(self.input_data)
+                # print(f"old map line {obstruction[1]} = {map[obstruction[1]]}")
+                map[obstruction[1]] = replace_char_at_index(map[obstruction[1]], obstruction[0], "#")
+                # print(f"new map line {obstruction[1]} = {map[obstruction[1]]}")
+                if self._is_loop(map, current_dir, current_pos):
+                    obstructions_placed.append(obstruction)
             current_dir, current_pos = self._move(current_dir, current_pos)
-            # look around if you can get back on the path with a turn to the right
-            test_dir = self._turn_right(current_dir)
-            test_pos = self._add_tuple(test_dir, current_pos)
-            if test_pos in visited_pos:
-                obstructions_placed += 1
-            # if current_pos == self._start_pos:
-            #     print(test_dir)
-            #     print(test_pos)
-            #     print(visited_pos)
-        return obstructions_placed  # ???
+            # print("new pos or dir")
+        # print(obstructions_placed)
+        return len(set(obstructions_placed))  # 252: too low, 1588 too low... 1740 right for diff set
+
+
+if __name__ == "__main__":
+    d = Day6()
+    print(f"== {str(d)} ====================")
+    print(f"    part 1: {d.part_1()}")
+    print(f"    part 2: {d.part_2()}")
+    print("FIN")
--- advent_of_code_2024/day_07/main.py
+++ advent_of_code_2024/day_07/main.py
@@ -1,12 +1,106 @@
+from typing import Callable
 from advent_of_code_2024.day import Day
+from advent_of_code_2024.common import pad_left
+
+
+def ternary(n):
+    if n == 0:
+        return "0"
+    nums = []
+    while n:
+        n, r = divmod(n, 3)
+        nums.append(str(r))
+    return ''.join(reversed(nums))
 
 
 class Day7(Day):
     def __init__(self):
         super().__init__(7, f"advent_of_code_2024/day_07/puzzle_input.txt")
 
+    def _add(self, x: int, y: int) -> int:
+        return x + y
+
+    def _mult(self, x: int, y: int) -> int:
+        return x * y
+
+    def _concat(self, x: int, y: int) -> int:
+        return int(str(x) + str(y))
+
+    def _parse_line(self, line: str) ->tuple[int, list[int]]:
+        first_t = line.split(": ")
+        answer = int(first_t[0])
+        args = list(map(int, first_t[1].split(" ")))
+        return answer, args
+
+    def _get_all_n_operator_combos(self, n: int) -> list[Callable]:
+        if n < 1:
+            raise ValueError("n must be at least 1")
+        bin_combos = [bin(i) for i in range(2 ** (n))]
+        combos = []
+        for _bin in [pad_left(str(s).lstrip("0b"), n) for s in bin_combos]:
+            callables = []
+            for b in _bin:
+                if b == "0":
+                    callables.append(self._add)
+                else:
+                    callables.append(self._mult)
+            combos.append(callables)
+        return combos
+
+    def _get_all_n_operator_combos_tern(self, n: int) -> list[Callable]:
+        if n < 1:
+            raise ValueError("n must be at least 1")
+        bin_combos = [ternary(i) for i in range(3 ** (n))]
+        combos = []
+        for _bin in [pad_left(str(s), n) for s in bin_combos]:
+            callables = []
+            for b in _bin:
+                if b == "0":
+                    callables.append(self._add)
+                elif b == "1":
+                    callables.append(self._mult)
+                else:
+                    callables.append(self._concat)
+            combos.append(callables)
+        return combos
+
+    def _evaluate_operations(self, operations: list[Callable], args: list[int]) -> int:
+        left_arg = args[0]
+        right_and_op = tuple(zip(args[1:], operations))
+        for right_arg, op in right_and_op:
+            left_arg = op(left_arg, right_arg)
+        return left_arg
+
     def part_1(self):
-        return super().part_1()
+        good_results = 0
+        for line in self.input_data:
+            answer, args = self._parse_line(line)
+            combos = self._get_all_n_operator_combos(len(args) - 1)
+            for combo in combos:
+                test_answer = self._evaluate_operations(combo, args)
+                # print(combo, args)
+                if test_answer == answer:
+                    good_results += answer
+                    break
+        return good_results  # 14711933466277
 
     def part_2(self):
-        return super().part_2()
+        good_results = 0
+        for line in self.input_data:
+            answer, args = self._parse_line(line)
+            combos = self._get_all_n_operator_combos_tern(len(args) - 1)
+            for combo in combos:
+                test_answer = self._evaluate_operations(combo, args)
+                # print(combo, args)
+                if test_answer == answer:
+                    good_results += answer
+                    break
+        return good_results  # 14711933466277
+
+
+if __name__ == "__main__":
+    d = Day7()
+    print(f"== {str(d)} ====================")
+    print(f"    part 1: {d.part_1()}")
+    print(f"    part 2: {d.part_2()}")
+    print("FIN")
